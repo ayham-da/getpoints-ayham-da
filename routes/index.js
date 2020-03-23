f1d7d325886d5c455e71450e103c4b77e2e1c35f
ayham <ayham.alaaeddin@gmail.com>
2020-03-23 11:20:28 +0100

    fertig

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+  res.status(200);
+  console.log("(Der Server funktioniert)")
+});
+
+router.get('/api',function(req,res,next){
+  res.json([{user:'fake', password:'fake'}])
+});
+
+module.exports = router;
